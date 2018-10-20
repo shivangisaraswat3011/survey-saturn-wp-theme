0ac08ca56fbdbae8299c9fb06756e5b7b7be75f1
shivangi saraswat <s.saraswat3019@gmail.com>
2018-10-20 14:25:29 +0530

    message

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package bootstrap2wordpress
+ */
+
+?>
+
+	
+
+<?php wp_footer(); ?>
+
+!-- Bootstrap core JavaScript
+            ================================================== -->
+            <!-- Placed at the end of the document so the pages load faster -->
+                <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+                <script src="<?php bloginfo('template_directoery'); ?>assets/js/jquery-3.3.1.min.js"></script>
+                <script src="<?php bloginfo('template_directoery'); ?>assets/js/bootstrap.min.js"></script>
+                
+                <script src="<?php bloginfo('template_directoery'); ?>assets/js/main.js"></script>
+                
+                <script type="<?php bloginfo('template_directoery'); ?>text/javascript" src="//use.typekit.net/gla7wnd.js"></script>
+                <script type="<?php bloginfo('template_directoery'); ?>text/javascript">try{Typekit.load();}catch(e){}</script>
+                
+                <!-- my tool kit-->
+				<link rel="stylesheet" href="<?php bloginfo('template_directoery'); ?>https://use.typekit.net/qbe4mnh.css">
+				
+</body>
+</html>
